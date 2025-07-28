fd5f9f64531591b12eebb9bd063b38dde4614d3b
VyshnaviKarakatla <vyshnavikarakatla@gmail.com>
2025-07-28 23:10:30 +0530

    branching_statements

---
+++ 05_control_statements/branching_statements.py
@@ -0,0 +1,16 @@
+#Branching statements
+#break
+for i in range(10):
+    if i==3:
+        break
+    print(i)
+
+#continue
+for i in range(10):
+    if i==3:
+        continue
+    print(i)
+
+#pass
+for i in range(10):
+    pass
